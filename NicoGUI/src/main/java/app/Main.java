2f7d702cc22c9dba4e3ae429e0630bfb00e44420
Nico <nico.esg.rey@gmail.com>
2024-12-28 14:14:15 +0000

    Modification to the directory organisation

--- NicoGUI/src/main/java/app/Main.java
+++ NicoGUI/src/main/java/app/Main.java
@@ -1,11 +1,8 @@
 package app;
 
-import java.awt.Frame;
-
 public class Main{
     public static void main(String[] args) {
         GUI gui = new GUI();
         gui.setVisible();
-        
     }
 }
